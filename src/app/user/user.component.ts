47a3823e637af77b1d551f30583b28aabee7a28a
baku67 <basile08@hotmail.fr>
2024-07-24 16:45:20 +0200

    dummy users list

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,4 +11,9 @@ import { Component } from '@angular/core';
 })
 export class UserComponent {
 
+  @Input() user:any;
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
 }
