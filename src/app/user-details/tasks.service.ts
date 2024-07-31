ce566b2e96f25a95c34c4ffc5d49438b142e5170
baku67 <basile08@hotmail.fr>
2024-07-31 22:45:18 +0200

    tasks -> localStorage

--- src/app/user-details/tasks.service.ts
+++ src/app/user-details/tasks.service.ts
@@ -5,7 +5,17 @@ import { NewTaskData, Task } from "./task/task.model";
 @Injectable({providedIn:'root'})
 export class TaskService {
 
+    // Dummytasks si pas de tasks dans le localStorage
     private tasks:Task[] = dummyTasks;
+
+    // Si tasks dans le localStorage -> chargement des tasks
+    constructor() {
+        const tasks = localStorage.getItem('tasks');
+
+        if(tasks) {
+            this.tasks = JSON.parse(tasks)
+        }
+    };
 
     getUserTasks(userId:string):Task[] {
         return this.tasks.filter((task) => task.userId === userId);
@@ -19,10 +29,17 @@ export class TaskService {
             summary: taskData.summary,
             dueDate: taskData.dueDate,
           });
+          this.saveTasks();
     }
 
     removeTask(taskId:string) {
         this.tasks = this.tasks.filter((task) => task.id !== taskId);
+        this.saveTasks();
+    }
+
+
+    private saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
     }
 
 
